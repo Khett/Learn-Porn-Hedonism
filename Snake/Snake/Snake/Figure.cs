369fae13d6b0093f2491e50a50c945a1da4406c5
Khett <keyc@mail.ru>
2016-05-02 22:08:45 +0600

    inheritance

---
+++ Snake/Snake/Snake/Figure.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    class Figure //Connect with HorisontalLine and VerticalLine
+    {
+        protected List<Point> pList;
+
+        public void Drow()
+        {
+            foreach (Point p in pList)
+            {
+                p.Draw();
+            }
+        }
+    }
+}
