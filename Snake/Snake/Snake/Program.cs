0ef96ae6df5f38232b1e0d499a29bb293de7db0a
Khett <keyc@mail.ru>
2016-06-12 13:28:27 +0600

    Hit

--- Snake/Snake/Snake/Program.cs
+++ Snake/Snake/Snake/Program.cs
@@ -12,54 +12,15 @@ namespace Snake
         static void Main(string[] args)
         {
 
-            VerticalLine v1 = new VerticalLine(0, 10, 5, '%');
-            Draw(v1);
-
-            Point p = new Point(4, 5, '*');
-            Figure fSnake = new Snake(p, 4, Direction.RIGHT);
-            Draw(fSnake);
-            Snake snake = (Snake)fSnake;
-
-            HorisontalLine h1 = new HorisontalLine(0, 5, 6, '&');
-
-            List<Figure> figures = new List<Figure>();
-            figures.Add(fSnake);
-            figures.Add(v1);
-            figures.Add(h1);
-
-            foreach(var f in figures)
-            {
-                f.Drow();
-            }
-       }
-
-        static void Draw(Figure figure)
-        {
-            figure.Drow();
-        }
-
-
-
             Console.SetBufferSize(80, 25);          //Set window size
 
-
-            // create frame
-            HorisontalLine upline = new HorisontalLine(0, 78, 0, '+' );
-            HorisontalLine downline = new HorisontalLine(0, 78, 24, '+' );
-
-            VerticalLine leftline = new VerticalLine(0, 24, 0, '+' );
-            VerticalLine rightline = new VerticalLine(0, 24, 78, '+' );
-
-            // paint frame
-            upline.Drow();
-            downline.Drow();
-            leftline.Drow();
-            rightline.Drow();
+            Walls walls = new Walls(80, 25);
+            walls.Draw();
 
             //  Drow point
             Point p = new Point(4, 5, '*');
             //  Make Snake
-            Snake snake = new Snake(p, 5, Direction.RIGHT);
+            Snake snake = new Snake(p, 2, Direction.RIGHT);
             snake.Drow();
 
             FoodCreator foodCreator = new FoodCreator(80, 25, '$');
@@ -68,7 +29,11 @@ namespace Snake
 
             while (true)
             {
-                if(snake.Eat(food))
+                if (walls.IsHit(snake) || snake.IsHitTail())
+                {
+                    break;
+                }
+                if (snake.Eat(food))
                 {
                     food = foodCreator.CreateFood();
                     food.Draw();
@@ -85,7 +50,9 @@ namespace Snake
                     ConsoleKeyInfo key = Console.ReadKey();
                     snake.HandleKey(key.Key);
                 }
+
             }
+
+        }     
+    }
         }
-    }   
-}
